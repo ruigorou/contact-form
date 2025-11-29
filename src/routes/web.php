b08722f9c82c90e8c893a5c1472fd21a15f79008
ruigorou <kai5903@gmail.com>
2025-11-29 14:17:56 +0900

    2day

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\ContactController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\IndexController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +16,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [IndexController::class, 'index']);
+
+Route::post('/contacts/confirm', [ContactController::class, 'confirm']);
